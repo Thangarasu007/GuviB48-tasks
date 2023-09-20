d25d61b16cff35ca714325674adf5f624c9ba954
Thangarasu N <135080555+Thangarasu007@users.noreply.github.com>
2023-09-20 13:18:21 +0530

    Add files via upload

---
+++ Task 3/script.js
@@ -0,0 +1,61 @@
+let myResume={
+    "basics": {
+      "name": "THANGARASU N",
+      "email": "maxbroklt@gamil.com",
+      "phone": 9629721477,
+      "degree": "B.E",
+      "location": {
+        "address": "18A Kadamber kovil, kaval kara street",
+        "postalCode": 639104,
+        "city": "Karur",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "website": "https://google.com",
+          "github":"https://github.com/Thangarasu007"
+        }
+      ]
+    },
+    "work": [
+      {
+        "company": "Smiths detection pvt ltd",
+        "position": "Field Service Engineer",
+        "startDate": "2022-08-19",
+        "endDate": "working still",
+        "summary": "i am an Field service Engineer",
+      },
+    ],
+    "education": [
+      {
+        "institution": "Kongunadu College Of Engineering",
+        "department": "ECE",
+        "studyType": "fulltime",
+        "batch start year": 2012,
+        "batch end year": 2017,
+        "gpa": 6.0,
+      }
+    ],
+    "skills": [
+      {
+        "name": "html,javascript",
+        "level": "beginer",
+        "project": [
+          "creating a professional company website"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "name": "Reading books, playing games,",
+      }
+    ]
+  }
+  console.log(myResume);
+  
